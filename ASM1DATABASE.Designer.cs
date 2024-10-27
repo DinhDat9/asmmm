ab2eadd70af5e5b226e2a9436eef8cbe92b886e9
DinhDat9 <Trinhdat0955@gmail.com>
2024-10-27 20:54:17 +0700

    Add files via upload

---
+++ ASM1DATABASE.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace ASMM
+{
+    partial class Product
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btsearch = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtselling = new System.Windows.Forms.TextBox();
+            this.txtqtt = new System.Windows.Forms.TextBox();
+            this.txtentry = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtcode = new System.Windows.Forms.TextBox();
+            this.dtgrv = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btsearch
+            // 
+            this.btsearch.Location = new System.Drawing.Point(569, 542);
+            this.btsearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btsearch.Name = "btsearch";
+            this.btsearch.Size = new System.Drawing.Size(331, 43);
+            this.btsearch.TabIndex = 39;
+            this.btsearch.Text = "Search";
+            this.btsearch.UseVisualStyleBackColor = true;
+            this.btsearch.Click += new System.EventHandler(this.btsearch_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Snow;
+            this.button5.Location = new System.Drawing.Point(1413, 561);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 25);
+            this.button5.TabIndex = 45;
+            this.button5.Text = "Back";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.SkyBlue;
+            this.button4.Location = new System.Drawing.Point(737, 492);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(713, 43);
+            this.button4.TabIndex = 38;
+            this.button4.Text = "Edit";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.button3.Location = new System.Drawing.Point(737, 446);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(713, 43);
+            this.button3.TabIndex = 35;
+            this.button3.Text = "delete";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(19, 444);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(713, 43);
+            this.button2.TabIndex = 34;
+            this.button2.Text = "Show";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1212, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 16);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Input Quantity";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1001, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 16);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "Selling Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(761, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 16);
+            this.label3.TabIndex = 40;
+            this.label3.Text = "Entry Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(395, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 16);
+            this.label2.TabIndex = 44;
+            this.label2.Text = "Product_Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Product Code";
+            // 
+            // txtselling
+            // 
+            this.txtselling.Location = new System.Drawing.Point(908, 80);
+            this.txtselling.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtselling.Name = "txtselling";
+            this.txtselling.Size = new System.Drawing.Size(236, 22);
+            this.txtselling.TabIndex = 32;
+            // 
+            // txtqtt
+            // 
+            this.txtqtt.Location = new System.Drawing.Point(1151, 80);
+            this.txtqtt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtqtt.Name = "txtqtt";
+            this.txtqtt.Size = new System.Drawing.Size(236, 22);
+            this.txtqtt.TabIndex = 33;
+            this.txtqtt.TextChanged += new System.EventHandler(this.txtqtt_TextChanged);
+            // 
+            // txtentry
+            // 
+            this.txtentry.Location = new System.Drawing.Point(668, 80);
+            this.txtentry.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtentry.Name = "txtentry";
+            this.txtentry.Size = new System.Drawing.Size(236, 22);
+            this.txtentry.TabIndex = 31;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(397, 80);
+            this.txtname.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(236, 22);
+            this.txtname.TabIndex = 30;
+            // 
+            // txtcode
+            // 
+            this.txtcode.Location = new System.Drawing.Point(153, 80);
+            this.txtcode.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtcode.Name = "txtcode";
+            this.txtcode.Size = new System.Drawing.Size(236, 22);
+            this.txtcode.TabIndex = 29;
+            // 
+            // dtgrv
+            // 
+            this.dtgrv.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dtgrv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgrv.Location = new System.Drawing.Point(249, 106);
+            this.dtgrv.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtgrv.Name = "dtgrv";
+            this.dtgrv.RowHeadersWidth = 62;
+            this.dtgrv.RowTemplate.Height = 28;
+            this.dtgrv.Size = new System.Drawing.Size(993, 334);
+            this.dtgrv.TabIndex = 37;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.Location = new System.Drawing.Point(19, 492);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(713, 43);
+            this.button1.TabIndex = 36;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Product
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1492, 646);
+            this.Controls.Add(this.btsearch);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtselling);
+            this.Controls.Add(this.txtqtt);
+            this.Controls.Add(this.txtentry);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtcode);
+            this.Controls.Add(this.dtgrv);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Product";
+            this.Text = "Product";
+            this.Load += new System.EventHandler(this.Product_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btsearch;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtselling;
+        private System.Windows.Forms.TextBox txtqtt;
+        private System.Windows.Forms.TextBox txtentry;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtcode;
+        private System.Windows.Forms.DataGridView dtgrv;
+        private System.Windows.Forms.Button button1;
+    }
+}
